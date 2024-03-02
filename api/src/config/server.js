cfeeb0df3535dff05723377131c1d22adf20e5c1
Douglas <douglasclaudino543@gmail.com>
2024-03-02 18:56:11 -0300

    Update server host configuration

--- api/src/config/server.js
+++ api/src/config/server.js
@@ -2,7 +2,7 @@
 const dateTime = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
 
 const serverConfig = {
-    host: process.env.SERVER_HOST || 'localhost',
+    host: process.env.SERVER_HOST || '0.0.0.0',
     port: process.env.PORT || 3333,
     dateTime: dateTime,
   };

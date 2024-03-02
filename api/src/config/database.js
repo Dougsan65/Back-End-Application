d110219d85554b45e1c8857ca2862b05142f361d
Douglas <douglasclaudino543@gmail.com>
2024-03-02 20:31:51 -0300

    Remove unused database and server configuration files

--- api/src/config/database.js
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config'
-import postgres from "postgres";
-
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, PGPORT} = process.env;
-const url = `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`;
-
-export const sql = postgres(url, {ssl: 'require'});
